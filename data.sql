386424f5999395e9ff0ab4bcb36af1a1dcea3cb7
Luis David Rodriguez Valades <luisdavidrodriguezv@gmail.com>
2022-10-18 20:38:48 -0500

    Update data.sql
    Fix Integer per Float

--- data.sql
+++ data.sql
@@ -10,13 +10,13 @@ VALUES
 
 INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg)
 VALUES
-('Charmander', '08-02-2020', 0, false, 11),
+('Charmander', '08-02-2020', 0, false, 11.0),
 ('Plantmon', '15-11-2021', 2, true, 5.7),
 ('Squirtle', '02-04-1993', 3, false, 12.13),
-('Angemon', '12-06-2005', 1, true, 45),
+('Angemon', '12-06-2005', 1, true, 45.0),
 ('Boarmon', '07-06-2005', 7, true, 20.4),
-('Blossom', '13-10-1998', 3, true, 17),
-('Ditto', '14-05-2022', 4, true, 22);
+('Blossom', '13-10-1998', 3, true, 17.0),
+('Ditto', '14-05-2022', 4, true, 22.0);
 
 //Change to unspecified
 BEGIN;
